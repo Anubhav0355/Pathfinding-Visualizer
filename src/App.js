904d1e8c4c26d5bbdd864a69ace4b13d6251c1b9
Anubhav0355 <anubhavsingh0355@gmail.com>
2024-07-06 14:41:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Visualiser from './Visualiser/Visualiser'
+
+function App() {
+  return (
+    <>
+      <Visualiser></Visualiser>
+    </>
+  );
+}
+
+export default App;
